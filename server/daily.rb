0cbc0bd9ccc51ce25cc5201b92be9b042ba9b217
Kofi Gumbs <h.kofigumbs@gmail.com>
2020-04-04 18:34:53 -0400

    Use real Daily.co API

--- server/daily.rb
+++ server/daily.rb
@@ -1,19 +1,26 @@
 # DAILY API
-#
+
 require "httparty"
 
-API_URL = "https://api.daily.co/v1"
-
 class Daily
-  def initialize(token)
-    @token = token
+  API_URL = "https://api.daily.co/v1"
+
+  # Create an API client from an API key <https://dashboard.daily.co/>
+  def initialize(key)
+    @key = key
   end
 
+  # See <https://docs.daily.co/reference#create-room>
   def create_room!
-    # TODO
-    {
-      "name" => "call-#{rand(256)}",
-      "url" => "https://hkgumbs.daily.co/hello",
-    }
+    post! "/rooms"
+  end
+
+  private
+
+  def post!(path)
+    HTTParty.post(API_URL + path, headers: {
+      "Content-Type" => "application/json",
+      "Authorization" => "Bearer " + @key,
+    })
   end
 end
