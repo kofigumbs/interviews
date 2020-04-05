144fe06c06fd443e84e0c355731fd3112b705604
Kofi Gumbs <h.kofigumbs@gmail.com>
2020-04-05 11:06:17 -0400

    Namespace shared globals

--- client/_includes/shared.js
+++ client/_includes/shared.js
@@ -1,27 +1,50 @@
-/* API SERVER BASE URL
+/* GLOBALS NAMESPACE
  */
-window.API = "{{ site.env['15SECONDLY_API'] }}";
+window.FIFTEEN_SECONDLY = {
+
+  /* API server base URL
+   */
+  API: "{{ site.env['15SECONDLY_API'] }}",
 
 
-/* MOMENT.JS IIFE WRAPPER
- *
- *   window.timeFormat : {
- *     forHumans : (timestamp: String) => String
- *     inSeconds : (timestamp: String) => Number
- *   }
- */
-window.timeFormat = (function() {
-  const parse = timestamp => moment(timestamp, "YYYY-MM-DD hh:mm:ss ZZZZ");
-  return {
-    forHumans: timestamp => parse(timestamp).calendar(),
-    inSeconds: timestamp => Math.round(parse(timestamp).valueOf() / 1000),
-  }
-})();
+  /* Moment.js IIFE wrapper
+   *
+   *   timeFormat : {
+   *     forHumans : (timestamp: String) => String
+   *     inSeconds : (timestamp: String) => Number
+   *   }
+   */
+  timeFormat: (function() {
+    const parse = timestamp => moment(timestamp, "YYYY-MM-DD hh:mm:ss ZZZZ");
+    return {
+      forHumans: timestamp => parse(timestamp).calendar(),
+      inSeconds: timestamp => Math.round(parse(timestamp).valueOf() / 1000),
+    }
+  })(),
 
-/* Bare minimum error feedback
- */
-window.viewError = function(node) {
-  node.innerHTML += `
-    <b style='color: darkred'>Sorry, but something here isn't quite right</b>
-  `;
-}
+  /* Listen for errors that may be thrown and report them
+   */
+  feedbackOnError: function(callback) {
+    try {
+      callback();
+    } catch (e) {
+      FIFTEEN_SECONDLY.viewError(e);
+    }
+  },
+
+  /* Bare minimum error view
+   */
+  viewError: function(e) {
+    let strong = document.getElementById("error-message");
+    // Skip most of the creation steps if this isn't the first error
+    if (!strong) {
+      strong = document.createElement("strong");
+      strong.id = "error-message";
+      strong.style.color = "darkred";
+      strong.innerText = "Sorry, but something went wrong!";
+      document.body.prepend(strong);
+    }
+    strong.title = e.toString();
+    strong.scollIntoView();
+  },
+};
